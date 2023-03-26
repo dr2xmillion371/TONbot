f457a0a157b2538bbc6596e986532f303b66bafd
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-26 18:57:41 -0500

    ton

---
+++ ton.py
@@ -0,0 +1,13 @@
+import time
+from time import *
+
+
+def ton():
+    ton_list=['0','C(0,0)','C(0,C(0,0))','C(0,C(0,C(0,0)))']#the list
+    for i in ton_list:
+        print(i)
+        time.sleep(0.01)
+
+
+
+ton()
