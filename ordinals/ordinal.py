880f5e4cdf7b566c6d05bac28ec363a5c2fb896f
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-26 19:02:21 -0500

    ordinal

---
+++ ordinals/ordinal.py
@@ -0,0 +1,19 @@
+import time
+from time import *
+
+
+def ordinals():
+  mat1='₁'
+  mat2='₂'
+  ordinals_list=['e'+mat1,'e'+mat2]
+  for i in ordinals_list:
+    print(i)
+    time.sleep(0.01)
+    
+    
+    
+
+    
+    
+    
+ordinals()
