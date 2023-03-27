2e9378fc639c7f928711c060ef3b8c264c36c138
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-26 19:09:22 -0500

    second

---
+++ ordinals/second.py
@@ -0,0 +1,17 @@
+import time
+import pprint
+from pprint import *
+from time import *
+from ordinal import ordinals
+
+
+ordinal=ordinals()
+
+
+
+
+def anotherWay():
+  moreordinals_list=['e','z','n']
+  moreordinals.insert(0,moreordinals[2:4])
+  ppr=pprint.PrettyPrinter(indent=2)
+  ppr.pprint(moreordinals)
