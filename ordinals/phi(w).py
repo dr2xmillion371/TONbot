98b6775ef24df8ef0b6a32fcc3a3b1004b810e93
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-04-02 18:56:44 -0500

    phi(ω)

---
+++ ordinals/phi(w).py
@@ -0,0 +1,13 @@
+import time
+from time import *
+
+def phi_ordinalCount():
+  phi_list=['w,','e_0','Z_0','φ(ω)']
+  for i in phi_list:
+    print(i)
+    time.sleep(0.01)
+    
+    
+
+    
+phi_ordinalCount()
