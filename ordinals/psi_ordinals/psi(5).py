6910a9b1260a5b8d6ee0feafe5dea31080020ee6
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-04-02 18:59:17 -0500

    psi(5)

---
+++ ordinals/psi_ordinals/psi(5).py
@@ -0,0 +1,15 @@
+import time
+from time import *
+
+def psi_ordinalCount():
+  psiList = ['ψ(5)']
+  for i in psiList:
+    print(i)
+    time.sleep(0.01)
+    
+    
+    
+    
+    
+    
+psi_ordinalCount()
